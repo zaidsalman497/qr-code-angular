11ad940bce6993918b67f7fc4bfe2bd26dbb7159
Zaid Salman <58643601+zaidsalman497@users.noreply.github.com>
2020-12-24 21:35:17 +1100

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
---
+++ src/js/common.js
@@ -0,0 +1,37 @@
+  
+function getElement(id) {
+    return document.getElementById(id);
+  }
+  
+  function setMessage(message) {
+    document.getElementById('status').textContent = message;
+  }
+  
+  
+  
+  function getCookieValue(name, value, expirydays) {
+    return `${name}= ${escape(value)}; expires= ${getExpiryTime(expirydays)}`;
+  }
+  
+  function getExpiryTime(number_of_days) {
+    var today = new Date();
+    var expire = new Date();
+    expire.setTime(today.getTime() + 60 * 60 * 1000 * 24 * number_of_days);
+    return expire.toGMTString();
+  }
+  
+  
+  function getCookieByName(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+  }
+  
+  function checkUser() {
+    if (!getCookieByName("loggedInUser")) {
+        window.location.href = "login.html";
+    } else {
+        document.getElementById("loginUserEmail").textContent = getCookieByName("loggedInUser");
+    }
+  
+  }
