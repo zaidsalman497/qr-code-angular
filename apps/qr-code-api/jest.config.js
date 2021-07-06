dc674040b5f6526c441bd51b11480f912033165d
Salman Aziz <salman.aziz@nintex.com>
2021-07-06 16:53:10 +1000

    chore: added nest js api for zaid

---
+++ apps/qr-code-api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'qr-code-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/qr-code-api',
+};
