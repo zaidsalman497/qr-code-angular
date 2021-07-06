ab57c61d8cb6cb999312945940ecb38215e0642f
Zaid Salman <zaidsalman497@gmail.com>
2021-07-06 11:02:31 +1000

    fixed payment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
+  stripe: {
+    testKey: 'pk_test_51J9MZbJ6E4w7cr4J7bYyZ67szJypiNxIRbJ7U3WtEsjS5mEM1juyVNZLxd4T7ZqBd1H85hxoyp56uHvLg5JMVz6900Zn3nO6tp'
+ },
   firebase: {
     apiKey: 'AIzaSyBC2WKMKDZmAaY6hpZIWkVdxeVVSlX4z84',
     authDomain: 'qr-code-website.firebaseapp.com',
