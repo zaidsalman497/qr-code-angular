f7bd67366dc362c8d3dc3e6ab449815c82024ff1
Zaid Salman <zaidsalman497@gmail.com>
2021-07-13 17:59:01 +1000

    not working payment comfirmed to firebase

--- apps/qr-code-angular/src/environments/environment.ts
+++ apps/qr-code-angular/src/environments/environment.ts
@@ -5,11 +5,7 @@
 export const environment = {
   production: false,
   stripe: {
-     testKey: 'pk_test_51J9MZbJ6E4w7cr4J7bYyZ67szJypiNxIRbJ7U3WtEsjS5mEM1juyVNZLxd4T7ZqBd1H85hxoyp56uHvLg5JMVz6900Zn3nO6tp',
-     baseUrl: 'https://localhost:4200/',
-      homeUrl: 'http://localhost:4200/home',
-      successUrl: 'http://localhost:4200/loggedin',
-      cancelUrl: 'http://localhost:4200/login',
+    stripe_key: 'pk_test_51J9MZbJ6E4w7cr4J7bYyZ67szJypiNxIRbJ7U3WtEsjS5mEM1juyVNZLxd4T7ZqBd1H85hxoyp56uHvLg5JMVz6900Zn3nO6tp'
     },
   firebase: {
     apiKey: 'AIzaSyBC2WKMKDZmAaY6hpZIWkVdxeVVSlX4z84',
