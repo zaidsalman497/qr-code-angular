13667e13a213d7f93a37dae93a60a209769f8652
Salman Aziz <salman.aziz@nintex.com>
2021-07-17 09:19:58 +1000

    wip

--- apps/qr-code-angular/src/app/payment/payment.component.ts
+++ apps/qr-code-angular/src/app/payment/payment.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentService } from './payment.service';
+
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
-  constructor(public payment: PaymentService) {
-  }
+  response: any;
+  ifPaid = false;
+  constructor(public payment: PaymentService) {}
   title = 'Zaid Pro';
   priceId = 'price_1JA42NJ6E4w7cr4JAdYjpcTw';
   product = {
     title: 'Zaid Pro',
     subTitle: 'pro',
-    description: 'Zaid Pro has now have courses and source codes and you can comment if you are stuck with something.',
-    price: 3.00
-    
+    description:
+      'Zaid Pro has now have courses and source codes and you can comment if you are stuck with something.',
+    price: 3.0,
   };
-  
-  quantity = 1;
- 
-  ngOnInit(): void {
-    
-  }
 
+  quantity = 1;
+
+  ngOnInit(): void {}
+
+  checkPayment() {
+    this.payment
+      .checkPayment()
+      .then((res) => {
+        this.response = res;
+      })
+      .catch((err) => {
+        this.response = err;
+      });
+  }
 }
--- apps/qr-code-angular/src/app/payment/payment.service.ts
+++ apps/qr-code-angular/src/app/payment/payment.service.ts
@@ -1,36 +1,28 @@
+import { PaymentComponent } from './payment.component';
 import { Injectable } from '@angular/core';
 import { loadStripe, SetupIntentResult, Stripe } from '@stripe/stripe-js';
 import { environment } from '../../environments/environment';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PaymentService  {
-  
-
+export class PaymentService {
   private stripePromise: Promise<Stripe | null>;
 
   constructor() {
     this.stripePromise = loadStripe(environment.stripe.stripe_key);
   }
 
-   async ifpaid() {
-    
+  async ifpaid() {
     return false;
-    const stripe = await this.stripePromise;
-    const result = await (await stripe?.confirmCardSetup(environment.stripe.stripe_key));
-    if(result?.error) {
-      return false;
-    }
-    return true;
   }
 
+  async checkPayment() {
+    const stripe = await this.stripePromise;
+    return await stripe?.confirmCardSetup(environment.stripe.stripe_key);
+  }
 
-  
-  async checkout(priceId = "price_1JA42NJ6E4w7cr4JAdYjpcTw", quantity = 1) {
- 
+  async checkout(priceId = 'price_1JA42NJ6E4w7cr4JAdYjpcTw', quantity = 1) {
     // Call your backend to create the Checkout session.
 
     // When the customer clicks on the button, redirect them to Checkout.
@@ -38,22 +30,19 @@ export class PaymentService  {
     const stripe = await stripePromise;
     const error = await stripe?.redirectToCheckout({
       mode: 'subscription',
-      lineItems: [{ price: priceId, quantity: quantity }],
+      lineItems: [{ price: priceId, quantity }],
       successUrl: 'http://localhost:4200/payment',
       cancelUrl: `http://localhost:4200/payment`,
-    })
-    
-    
+    });
+
     // If `redirectToCheckout` fails due to a browser or network
     // error, display the localized error message to your customer
     // using `error.message`.
-    
-    if (error) {
+    return this.ifpaid();
+    /* if (error) {
       console.log('sorry we could not complete your payment because' + error);
     } else {
-      this.ifpaid()
-    }
+      
+    }*/
   }
-  
-
 }
